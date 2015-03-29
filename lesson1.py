a10f0784a7e0d9ffb935f6b66be28469ba84eed0
Karl Brown <karl@thekarlbrown.com>
2015-03-28 23:47:51 -0400

    First Python Tutorial

---
+++ lesson1.py
@@ -0,0 +1,24 @@
+- Python Lesson 1 - 
+By Karl Brown
+
+Here is a series of shorthand tutorials to help individuals learn Python. I will be improving them as more individuals attempt them, and extending them as interest forces me to reach the end of my lessons.
+
+- Tools -
+http://learnpython.org/ - Guide that helps me progress my lessons
+http://notepad-plus-plus.org/ - Good way to view code in Windows
+http://repl.it/ - Way to view the code you write online (Python Interpreter)
+http://www.cheatography.com/davechild/cheat-sheets/python/ - Cheat Sheet (for experienced users)
+
+Variables in real life: someone says "hey , lets go to dinner at 5:00 pm"
+The computer must save the data somehow
+A variable is means of saving that data, just like the human brain remembers sentences, numbers, etc
+So 'hey, lets go to dinner at 5:00 pm' would be a string. it could have the name 'karlconvo' set to the value of the following characters 'hey, lets go to dinner at 5:00 pm'
+If you asked me what time dinner was, and I said 5pm, it could be an integer with the name 'dinnertime' set to the value of '5'
+If you asked what time dinner was and you said 5:15 pm, the time could be stored as a float named "dinnertime" with the value '5.15' (if you so choose to store it that way)
+Alternatively, you could use 17.15 as it is pm and the day is 24 hours
+Integers are whole numbers, floats can have decimals
+Everything is an object in python, including variables.
+Quotes are how the computer can tell if you are typing in code to be interpretted (typing in code) or just typing it text to be set to a value (setting a string, using text humans understand not code)
+
+HW#1
+customized hello world - Type a string that says "Although a teacher can be nice, Hunter knows the internet is the best tool"
